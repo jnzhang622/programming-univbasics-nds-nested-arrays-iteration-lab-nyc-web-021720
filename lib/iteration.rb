ff265c9eff11f965f9e202530932c5158897e715
jolly-assembler-1925 <jnzhang622@gmail.com>
2020-02-05 06:41:01 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -24,7 +24,11 @@ def find_greater_pair(src)
   final = []
    count = 0
    while count < src.size do
-     if  src[count][0] < src[count][1] 
+     if  src[count][0] > src[count][1] 
+       final << src[count][0]
+    else
+      final src[count][1]
+      
      count = count + 1
    end
    return final
